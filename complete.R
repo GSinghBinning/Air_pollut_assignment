6f4b9bbfbd06e83366104e4f3f8ac1c9bd943265
GSinghBinning <g.singh.binning@gmail.com>
2020-04-12 17:30:32 +0530

    renaming function

--- complete.R
+++ complete.R
@@ -1,11 +1,9 @@
-complete1 <- function(directory, id = 1:332){
+complete <- function(directory, id = 1:332){
   fp <- list.files(c(directory,"/",id,".csv"), pattern =".csv", full.names = TRUE)
   afp <- fp[id]
-  
   df <- data.frame(Date=as.Date(character()), File=character(), 
                    User=character(), stringsAsFactors=FALSE) 
   names(df) <- c("id","nobs")
-  
   result <- mapply(readdata, id,afp,SIMPLIFY = TRUE)
   result_asdf <- as.data.frame(t(result))
   print(result_asdf)
